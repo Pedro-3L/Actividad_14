03a1dfa42f563a59a505515f268f898cb9b03686
Pedro-3L <lando133@gmail.com>
2026-03-17 19:08:38 -0600

    Adicion ejercicio 8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<Cuenta> cuentas = new List<Cuenta>();
+Console.Write("Ingrese cuantas cuentas desea registrar: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+	Cuenta c = new Cuenta();
+	Console.WriteLine();
+	Console.WriteLine($"Cuenta #{i + 1}");
+
+	Console.Write("Titular: "); 
+	c.Titular = Console.ReadLine();
+
+	Console.Write("Saldo inicial: ");
+	c.Saldo = double.Parse(Console.ReadLine());
+
+	Console.Write("Monto a depositar: ");
+	double deposito = double.Parse(Console.ReadLine());
+	c.Depositar(deposito);
+
+	Console.Write("Monto a retirar: ");
+	double retiro = double.Parse(Console.ReadLine());
+
+	bool retiroExitoso = c.Retirar(retiro);
+
+	if (!retiroExitoso)
+	{
+		Console.WriteLine("No se pudo realizar el retiro por saldo insuficiente.");
+	}
+	cuentas.Add(c);
+}
+Console.Clear();
+Console.WriteLine("==== CUENTAS REGISTRADAS ====");
+foreach (Cuenta c in cuentas)
+{
+	c.MostrarDatos();
+}
+
+class Cuenta
+{
+	public string Titular;
+	public double Saldo;
+	public void Depositar(double monto) 
+	{ 
+		Saldo += monto;
+	}
+	public bool Retirar(double monto)
+	{
+		if (monto <= Saldo) 
+		{ 
+			Saldo -= monto; return true;
+		}
+		return false;
+	}
+	public void MostrarDatos()
+	{
+		Console.WriteLine($"Titular: {Titular} | Saldo actual: Q{Saldo:F2}");
+	}
+}
